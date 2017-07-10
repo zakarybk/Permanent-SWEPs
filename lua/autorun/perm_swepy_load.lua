4bda22dd2afcc7f8ac86b136931b4cccec20d915
Hackcraft <zakary@zakary.org>
2017-07-10 01:12:28 +0100

    Update perm_swepy_load.lua

--- lua/autorun/perm_swepy_load.lua
+++ lua/autorun/perm_swepy_load.lua
@@ -1,6 +1,9 @@
 if SERVER then
+	AddCSLuaFile("perm_swepy/shared_perm_sweps.lua")
 	AddCSLuaFile("perm_swepy/cl_perm_sweps.lua")
+	include("perm_swepy/shared_perm_sweps.lua")
 	include("perm_swepy/sv_perm_sweps.lua")
 else
+	include("perm_swepy/shared_perm_sweps.lua")
 	include("perm_swepy/cl_perm_sweps.lua")
 end
